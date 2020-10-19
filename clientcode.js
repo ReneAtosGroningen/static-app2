b6b8b23c16c991c3c71447f757382faadce3e923
René Hartsema <rene.hartsema@atos.net>
2020-10-19 08:42:54 +0200

    meerdere aanvullingen

--- clientcode.js
+++ clientcode.js
@@ -1,4 +1,30 @@
+//--------------------------------------------------
+//JavaScript code voor de 'Galgje' static website
+//Oktober 2020, Rene Hartsema
+//--------------------------------------------------
+
+// API url
+gameUrl = "/api/playgame";
+gameUrlComplete = "https://red-field-03d09a603.azurestaticapps.net/api/playgame";
+
 function SendCharacter() {
 
     alert("Hi");
 }
+
+function StartNewGame() {  
+    
+    $.ajax({
+        method: "GET",
+        url: "https://red-field-03d09a603.azurestaticapps.net/api/playgame",
+        contentType: "application/json",
+        dataType: 'json'
+    }).done(function (res) {
+        alert(res);
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+        alert("AJAX call failed: " + textStatus + ", " + errorThrown);
+    });
+
+
+}
+
